ca7d566cf390c433d7505feba56603fd3a56298a
Watson <joshua.watson06@gmail.com>
2020-02-25 11:18:23 -0600

    Initial Commit

---
+++ Binder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Binder
+//
+//  Created by Joshua Watson on 2/25/20.
+//  Copyright © 2020 Joshua Watson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
